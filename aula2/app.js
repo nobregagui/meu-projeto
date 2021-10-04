d0a70c7a115ac4a731cf2644d6f98a43beaceff0
Guilherme Nobrega <nobregagui8@gmail.com>
2021-10-04 11:45:00 -0300

    Alterado com sucesso

--- aula2/app.js
+++ aula2/app.js
@@ -1,5 +1,19 @@
 const http = require('http')
+const express = require('express')
+const app = express()
 
 http.createServer((req, res) => {
-    console.log("Servidor está rodandno")
-}).listen(3030)
+   res.writeHead(200, {"Content-type":"text/plain"})
+        switch (req.url) {
+            case "/":
+                res.end("Você está na página home")
+                break;
+            case "/contato":
+                res.end("Você está na página contato")
+            default:
+                res.end('Sem resultados')
+                break;
+        }
+
+   res.end("Meu primeiro servidor")
+}).listen(3000)
