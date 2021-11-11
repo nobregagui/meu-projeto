bea518aa1d1f1d126fd83c89c2a1178d98917b62
Guilherme Nobrega <nobregagui8@gmail.com>
2021-11-11 16:07:22 -0300

    Adicionado novas atualizações projeto Integrador

--- aula2/projetoIntegrador/funcoes.js
+++ aula2/projetoIntegrador/funcoes.js
@@ -42,3 +42,32 @@ class MobileNavbar {
     ".nav-list li",
   );
   mobileNavbar.init();
+
+  let time = 5000,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#slider img")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
